d539c11c213170cbb158a9c9d5a19d0c7500a48e
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2025-09-10 23:14:36 +0700

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-blue-600">
+          Hello, React + Vite + Tailwind v4 🎉
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
